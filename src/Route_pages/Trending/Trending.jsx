bcb5eba1ba0da46af770a1e558daf15044ea6aa9
20100850-ganesh <20100850@mail.wit.ie>
2023-04-10 19:25:47 +0530

    Completed Trending with styling

--- src/Route_pages/Trending/Trending.jsx
+++ src/Route_pages/Trending/Trending.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Singlecontent from '../../components/singlecontent/Singlecontent'
+import './Trending.css'
 
 const Trending = () => {
 
